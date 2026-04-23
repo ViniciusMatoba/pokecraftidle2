642d37ca57b518ea894d61d4d54a86ec1788404f
Vinícius Matoba <seikivinicius@gmail.com>
2026-04-23 13:40:45 -0300

    v1.6.17: final version display update

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,5 +1,5 @@
-export const APP_VERSION = '1.6.16';
-export const APP_VERSION_DATE = '2026-04-23 13:31';
+export const APP_VERSION = '1.6.17';
+export const APP_VERSION_DATE = '2026-04-23 13:40';
 
 export const NATURE_LIST = ['Adamant', 'Modest', 'Jolly', 'Timid', 'Bold', 'Calm', 'Impish', 'Careful', 'Brave', 'Quiet'];
 
