217bab970298d9d50883eb167e3c8292cb981aad
Vinicius Matoba <seikivinicius@gmail.com>
2026-05-01 13:44:02 -0300

    chore: atualizar versao 1.50.4

--- src/constants/version.js
+++ src/constants/version.js
@@ -1,3 +1,3 @@
-export const APP_VERSION = '1.50.3';
-export const VERSION = '1.50.3';
-export const APP_VERSION_DATE = '01/05/2026 09:45';
+export const APP_VERSION = '1.50.4';
+export const VERSION = '1.50.4';
+export const APP_VERSION_DATE = '01/05/2026 13:43';
