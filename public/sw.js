3a4d97e49e392670c4303a583d2e004ef325498a
Vinícius Matoba <seikivinicius@gmail.com>
2026-05-04 12:30:41 -0300

    Fix SW cache version and force clear on update button

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'pokecraft-cache-v1.50.8';
-const RUNTIME_CACHE = 'pokecraft-runtime-v1.50.8';
+const CACHE_NAME = 'pokecraft-cache-v1.51.3';
+const RUNTIME_CACHE = 'pokecraft-runtime-v1.51.3';
 const STATIC_ASSETS = [
   './',
   './index.html',
